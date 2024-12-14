65a3912cafa02b80b97300a26b4a1ad33d130700
brazhen <naglis.brazenas@mif.stud.vu.lt>
2024-12-14 18:47:17 +0200

    Add Doxygen documentation

---
+++ docs/search/all_14.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['queue_0',['Queue',['../class_queue.html',1,'']]],
+  ['queuenode_1',['QueueNode',['../class_queue_node.html',1,'']]],
+  ['quickly_2',['Quickly',['../md_googletest_2docs_2gmock__cook__book.html#autotoc_md180',1,'Writing New Action Templates Quickly'],['../md_googletest_2docs_2gmock__cook__book.html#NewMatchers',1,'Writing New Matchers Quickly'],['../md_googletest_2docs_2gmock__cook__book.html#autotoc_md172',1,'Writing New Parameterized Matchers Quickly']]],
+  ['quickstart_3a_20building_20with_20bazel_3',['Quickstart: Building with Bazel',['../md_googletest_2docs_2quickstart-bazel.html',1,'']]],
+  ['quickstart_3a_20building_20with_20cmake_4',['Quickstart: Building with CMake',['../md_googletest_2docs_2quickstart-cmake.html',1,'']]]
+];
